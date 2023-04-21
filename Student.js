4c5d78c9d244dd8193e55980c6dcd2daea049d55
karthik <karthikkn877@gmail.com>
2023-04-21 15:15:56 +0530

    committing my exceelelent coddee

---
+++ Student.js
@@ -0,0 +1,11 @@
+const student = ['Ram', 'Bheem', 'Legend'];
+const marks  = [56,67,44]
+
+console.log(student);
+module.exports = 'helloo vanakkam';
+
+module.exports = student;
+
+module.exports = {
+  student, marks
+};
